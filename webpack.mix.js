872527830e8bc2daf1665d37d1f1db01e49c69a2
aziyan99 <rajaazian08@gmail.com>
2022-02-14 17:16:49 +0700

    add source map

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
+    .sourceMaps()
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
-    ]);
+    ])
+    .sourceMaps();
 
-   mix.browserSync('127.0.0.1:8000');
+mix.browserSync('127.0.0.1:8000');
